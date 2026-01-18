e548a650e1a7f1d0e2fb46d9bae17e223adce3f1
Federico Arzuaga <fedearzuaga@Federicos-MacBook-Air.local>
2026-01-18 17:44:35 -0300

    Finished the first implementation of uploadThing

--- src/app/dashboard/components/UserUploadProfilePicture.tsx
+++ src/app/dashboard/components/UserUploadProfilePicture.tsx
@@ -1,5 +1,8 @@
 import { useUploadThing } from "@/utils/uploadthing"
 import { useRouter } from "next/navigation";
+import { IoCameraOutline } from "react-icons/io5";
+import { LuLoaderCircle } from "react-icons/lu";
+import style from './UserUploadProfilePicture.module.css'
 
 interface Props {
     id: string
@@ -10,7 +13,7 @@ export const UserUploadProfilePicture = ({ id }: Props) => {
     const router = useRouter()
 
     const { startUpload, isUploading } = useUploadThing("profilePicture", {
-        onClientUploadComplete(res) {
+        onClientUploadComplete: async (res) => {
             await fetch(`/api/user/profile-picture/${id}`, {
                 method: 'POST',
                 headers: {
@@ -27,6 +30,26 @@ export const UserUploadProfilePicture = ({ id }: Props) => {
     });
 
     return (
-        
+        <label className="absolute bottom-0 w-10 h-10 right-0 bg-white p-2 rounded-full cursor-pointer hover:bg-gray-100 transition-colors flex justify-center items-center">
+            <span className="text-white text-xs">
+                {
+                    isUploading 
+                        ? (
+                            <LuLoaderCircle className={`text-teal-500 ${style.spinner}`} size={25} />
+                        )
+                        : (
+                            <IoCameraOutline className="text-teal-500" size={25} />
+                        )
+                }
+            </span>
+            <input 
+                type="file" 
+                className="hidden" 
+                onChange={async (e) => {
+                    const file = e.target.files?.[0];
+                    if (file) await startUpload([file]);
+                }}
+            />
+        </label>
     )
 }
